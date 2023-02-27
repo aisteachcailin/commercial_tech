82f5c7168be4a224e221e2fec0900beb38f25005
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-02-27 11:15:33 +0300

    fix header's and slider's styles, separation swiper js

--- src/js/main.js
+++ src/js/main.js
@@ -8,56 +8,30 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+const swiperProduct = new Swiper('.swiper-product', {
   // configure Swiper to use modules
-  modules: [Navigation, Pagination],
+  modules: [Navigation],
   direction: 'horizontal',
-  loop: false,
-  //   breakpoints: {
-//     320: {
-//       slidesPerView: 1,
-//       spaceBetween: 8
-//     },
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 14
-//     },
-//     1280: {
-//       slidesPerView: 2,
-//       spaceBetween: 18
-//     }
-//   }
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
   slidesPerView: 4,
   spaceBetween: 20,
 
 });
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const swiperBanner = new Swiper('.swiper-banner', {
+  modules: [Pagination],
+  direction: 'horizontal',
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  slidesPerView: 1,
+
+});
