ed5ec6829bf6d8378e8990c986478eff95779b5a
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-03-05 21:03:44 +0300

    fancybox popup for call link

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import '../scss/style.scss';
-import $ from "jquery";
+import '../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
+import { Fancybox } from "@fancyapps/ui";
 import {banner, product, certificate} from './modules/slider';
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
